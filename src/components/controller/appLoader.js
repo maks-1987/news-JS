d59aac0fc9decd6b3f0a7cd04f30ea038020871c
MaxD <max.moderation@gmail.com>
2022-06-27 10:29:15 +0300

    feat: add my own api-key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ed688cb45171420d8b743a63b310b8fc', // получите свой ключ https://newsapi.org/
         });
     }
 }
