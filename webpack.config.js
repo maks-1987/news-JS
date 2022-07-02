24a44d302319ed7b16334abf05494e810736ac7b
MaxD <max.moderation@gmail.com>
2022-07-02 15:07:59 +0300

    feat: fix config files

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const baseConfig = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        publicPath: 'dist',
+        // publicPath: './dist',
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
     },
