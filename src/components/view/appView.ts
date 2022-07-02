b139957ce2b69569e61de3a48aaa4cc533bc61ba
MaxD <max.moderation@gmail.com>
2022-07-02 22:23:52 +0300

    feat: TS migration file appView.ts

---
+++ src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import { IAppView, Response, ResponseSources } from '../../types/types';
+import News from './news/news';
+import Sources from './sources/sources';
+
+export class AppView implements IAppView {
+  news: News;
+  sources: Sources;
+
+  constructor() {
+    this.news = new News();
+    this.sources = new Sources();
+  }
+
+  drawNews(data: Response) {
+    const values = data?.articles ? data?.articles : [];
+    this.news.draw(values);
+  }
+
+  drawSources(data: ResponseSources) {
+    const values = data?.sources ? data?.sources : [];
+    this.sources.draw(values);
+  }
+}
+
+export default AppView;
