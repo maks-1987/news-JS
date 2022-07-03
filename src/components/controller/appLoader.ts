da53d4383e5d5e435bf764dcffc9ebca7638b94d
MaxD <max.moderation@gmail.com>
2022-07-03 18:29:15 +0300

    feat: TS migrate file appLoader.ts

---
+++ src/components/controller/appLoader.ts
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: 'ed688cb45171420d8b743a63b310b8fc', // получите свой ключ https://newsapi.org/
+    });
+  }
+}
+
+export default AppLoader;
