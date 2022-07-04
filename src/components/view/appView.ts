6c6112ff85dc1df50f3632b700938b13664480be
MaxD <max.moderation@gmail.com>
2022-07-04 17:39:22 +0300

    feat: migrate appView.ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,4 +1,4 @@
-import { IAppView, ResponseData, ResponseSources } from '../../types/types';
+import { IAppView, EverythingResponse, SourcesResponse } from '../../types/types';
 import News from './news/news';
 import Sources from './sources/sources';
 
@@ -11,12 +11,12 @@ export class AppView implements IAppView {
     this.sources = new Sources();
   }
 
-  drawNews(data: ResponseData) {
+  drawNews(data: EverythingResponse) {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data: ResponseSources) {
+  drawSources(data: SourcesResponse) {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
